62b664da71b9e45c74a3b64a9c390b8c13dc27eb
disksing <i@disksing.com>
2022-05-08 17:11:28 +0800

    fix line length issue
    Signed-off-by: disksing <i@disksing.com>

--- main.py
+++ main.py
@@ -10,7 +10,11 @@ scene.set_floor(-20/64, (1, 1, 1))
 @ti.kernel
 def initialize_voxels():
     for i, j, k in ti.ndrange((0, 81), (0, 81), (0, 81)):
-        if (int(i//27!=1)+int(j//27!=1)+int(k//27!=1))>=2 and (int((i%27)//9!=1)+int((j%27)//9!=1)+int((k%27)//9!=1))>=2 and (int((i%9)//3!=1)+int((j%9)//3!=1)+int((k%9)//3!=1))>=2 and (int((i%3)!=1)+int((j%3)!=1)+int((k%3)!=1))>=2:
+        draw = True
+        for x in ti.static([3, 9, 27, 81]):
+            if int((i%x)//(x//3)==1)+int((j%x)//(x//3)==1)+int((k%x)//(x//3)==1) >= 2:
+                draw = False
+        if draw:
             scene.set_voxel(vec3(i-20, j-20, k-20), 1, vec3(0.9, 0.3, 0.3))
 
 initialize_voxels()
